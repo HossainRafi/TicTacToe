491623aab3338be5f5bfb3aa0dc70b66c25ee0aa
HossainRafi <hossainrafi71@gmail.com>
2022-11-10 20:43:44 +0600

    click function add for x

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import { Board } from "./components/Board/Board";
+import { ScoreBoard } from './components/ScoreBoard/ScoreBoard';
+import { ResetButton } from './components/ResetButton/ResetButton';
 
 function App() {
-  const board = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleBoxClick = (boxIdx) => {
+    // Step 1: Update the board
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    });
+
+    setBoard(updatedBoard);
+
+  };
   return (
-    <div>
-      <Board board={board} />
+    <div className="App">
+      <Board board={board} onClick={null} />
     </div>
   );
 }
