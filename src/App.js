b614055897141e0fd6ed33e34166ff44fa18c76d
HossainRafi <hossainrafi71@gmail.com>
2022-11-10 19:30:52 +0600

    box component done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
+import { Box } from "./components/Box/Box";
 
 function App() {
+  const board= {"x", "x", "x", "x", "x", "x", "x", "x", "x",}
   return (
     <div>
-      <h1>Hello Everyone</h1>
-      <h1>
-        This Project Contains The Basic Setup Of A New React Project. You Need
-        To Change Only The Title & Favicon Of This Project
-      </h1>
+      <Box value="x" onClick={null} />
     </div>
   );
 }
